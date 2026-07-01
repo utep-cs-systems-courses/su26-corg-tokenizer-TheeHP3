7fcbf5acc09ea10c0d1ff5cb865c9b595fc01b0d
TheeHP3 <hpedroza@miners.utep.edu>
2026-06-30 20:05:02 -0600

    Updated uimain.c to allow user to input their own strings for tests

--- src/uimain.c
+++ src/uimain.c
@@ -1,30 +1,186 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <tokenizer.h>
 
 int main()
 {
-    char str[] = "  hello   world  test ";
+    char input[256];
+    int choice;
 
-    printf("Tokens: %d\n", count_tokens(str));
+    while (1) {
+        printf("\n===== TOKENZIER =====\n");
+        printf("1. Test space_char()\n");
+        printf("2. Test non_space_char()\n");
+        printf("3. Test token_start()\n");
+        printf("4. Test token_terminator()\n");
+        printf("5. Test count_tokens()\n");
+        printf("6. Test tokenize()\n");
+        printf("7. Test copy_str()\n");
+        printf("0. Quit\n");
+        printf("> ");
 
-    char *start = token_start(str);
+        scanf("%d", &choice);
+        getchar();      // remove newline after scanf
 
-    if (start)
-        printf("First token begins with: %c\n", *start);
+        switch (choice) {
 
-        printf("space_char(' ') = %d\n", space_char(' '));
-        printf("space_char('a') = %d\n", space_char('a'));
+        case 1:
+            printf("Enter one character: ");
+            scanf("%c", &input[0]);
+            getchar();
 
-        printf("non_space_char('a') = %d\n", non_space_char('a'));
-        printf("non_space_char(' ') = %d\n", non_space_char(' '));
+            printf("space_char('%c') = %d\n",
+                   input[0], space_char(input[0]));
+            break;
 
-        printf("count_tokens = %d\n", count_tokens(str));
+        case 2:
+            printf("Enter one character: ");
+            scanf("%c", &input[0]);
+            getchar();
 
-    	char input[] = "this is a test";
-    	char **tokens = tokenize(input);
+            printf("non_space_char('%c') = %d\n",
+                   input[0], non_space_char(input[0]));
+            break;
 
-    	print_tokens(tokens);
-    	free_tokens(tokens);
+        case 3: {
+            printf("Enter a string:\n> ");
+            fgets(input, sizeof(input), stdin);
+
+            // Remove newline
+            int i = 0;
+            while (input[i] != '\0') {
+                if (input[i] == '\n') {
+                    input[i] = '\0';
+                    break;
+                }
+                i++;
+            }
+
+            char *start = token_start(input);
+
+            if (start)
+                printf("First token starts with: %c\n", *start);
+            else
+                printf("No tokens found.\n");
+
+            break;
+        }
+
+        case 4: {
+            printf("Enter a string:\n> ");
+            fgets(input, sizeof(input), stdin);
+
+            // Remove newline
+            int i = 0;
+            while (input[i] != '\0') {
+                if (input[i] == '\n') {
+                    input[i] = '\0';
+                    break;
+                }
+                i++;
+            }
+
+            char *start = token_start(input);
+
+            if (start) {
+                char *end = token_terminator(start);
+
+                printf("First token: ");
+
+                while (start < end) {
+                    putchar(*start);
+                    start++;
+                }
+
+                putchar('\n');
+            }
+            else {
+                printf("No tokens found.\n");
+            }
+
+            break;
+        }
+
+        case 5:
+            printf("Enter a string:\n> ");
+            fgets(input, sizeof(input), stdin);
+
+            // Remove newline
+            int i = 0;
+            while (input[i] != '\0') {
+                if (input[i] == '\n') {
+                    input[i] = '\0';
+                    break;
+                }
+                i++;
+            }
+
+            printf("Number of tokens = %d\n",
+                   count_tokens(input));
+
+            break;
+
+        case 6: {
+            printf("Enter a string:\n> ");
+            fgets(input, sizeof(input), stdin);
+
+            // Remove newline
+            int i = 0;
+            while (input[i] != '\0') {
+                if (input[i] == '\n') {
+                    input[i] = '\0';
+                    break;
+                }
+                i++;
+            }
+
+            char **tokens = tokenize(input);
+
+            printf("\nTokens:\n");
+            print_tokens(tokens);
+
+            free_tokens(tokens);
+
+            break;
+        }
+
+        case 7: {
+            printf("Enter a string:\n> ");
+            fgets(input, sizeof(input), stdin);
+
+            // Remove newline
+            int i = 0;
+            while (input[i] != '\0') {
+                if (input[i] == '\n') {
+                    input[i] = '\0';
+                    break;
+                }
+                i++;
+            }
+
+            int len;
+
+            printf("Enter number of characters to copy: ");
+            scanf("%d", &len);
+            getchar();
+
+            char *copy = copy_str(input, len);
+
+            printf("Copied string: %s\n", copy);
+
+            free(copy);
+
+            break;
+        }
+
+        case 0:
+            printf("Goodbye!\n");
+            return 0;
+
+        default:
+            printf("Invalid option.\n");
+        }
+    }
 
     return 0;
 }
